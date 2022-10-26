9af7b3404b69de3b0d3b2402529ffe44046fc56a
Marcus Vinícius <marcusv892@gmail.com>
2022-10-25 22:55:59 -0300

    Template page and script calculate IMC

---
+++ script.js
@@ -0,0 +1,10 @@
+const form = document.querySelector('form')
+const inputWeight = document.querySelector('#weight')
+const inputHeight = document.querySelector('#height')
+
+form.onsubmit = (event) => {
+   event.preventDefault()
+
+   const weight = inputWeight.value 
+   
+}
