3b7c7e3ab42d505d4ef1e4b4f2e6f121aa9ada8b
Marcus Vinícius <marcusv892@gmail.com>
2022-11-02 13:39:35 -0300

    Resolved function calculate IMC and modal exibition

--- script.js
+++ script.js
@@ -1,10 +1,32 @@
 const form = document.querySelector('form')
 const inputWeight = document.querySelector('#weight')
 const inputHeight = document.querySelector('#height')
+const modal = document.querySelector('.modal-wrapper')
+const btnCloseModal = document.querySelector('.close')
+
+btnCloseModal.addEventListener('click', handleCloseModal)
 
 form.onsubmit = (event) => {
    event.preventDefault()
 
-   const weight = inputWeight.value 
+   const weight = Number(inputWeight.value)
+   const height = Number(inputHeight.value)
+
+   const resultImc = calcImc(weight, height)
+
+   modal.querySelector('span').innerText =`Seu IMC é de ${resultImc}`
+   modal.classList.add('open')
+}
+
+
+function calcImc(weight, height){
    
+   
+   return (weight / ((height / 100) ** 2)).toFixed(2)
+}
+
+
+function handleCloseModal(){
+   modal.classList.remove('open')
+   form.reset();
 }
